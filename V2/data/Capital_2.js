a6d5871b195d7a73cfd5ee6ba128a14133b388e3
zquarter <47930896+zquarter@users.noreply.github.com>
2019-02-24 12:39:17 -0800

    Add files via upload

---
+++ V2/data/Capital_2.js
@@ -0,0 +1,8 @@
+var json_Capital_2 = {
+"type": "FeatureCollection",
+"name": "Capital_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Washington, D.C.", "pop_max": 4338000.0 }, "geometry": { "type": "Point", "coordinates": [ -77.011364439437159, 38.901495235086998 ] } }
+]
+}
